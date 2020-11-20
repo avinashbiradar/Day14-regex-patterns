04ccbf7a4918c0bc020446548596d7c093458bd5
avinashbiradar <avinashbiradar179@gmail.com>
2020-11-20 14:10:20 +0530

    Added:regex first-name use case

--- regex.sh
+++ regex.sh
@@ -1,2 +1,12 @@
 echo "Regex Patterns Assignment "
 
+
+echo "First Name : "
+read name
+pat="^[A-Z a-z]{2,}"
+if [[ $name == $pat ]]
+then
+  echo "$name is valid Name"
+else
+  echo "Not valid Name"
+fi
