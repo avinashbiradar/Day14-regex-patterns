3b5a49dc9388b79d3211e15e98d5532d0a870f90
avinashbiradar <avinashbiradar179@gmail.com>
2020-11-20 14:39:24 +0530

    Added:regex Email use case

--- regex.sh
+++ regex.sh
@@ -1,3 +1,18 @@
+echo "Regex Patterns Assignment "
+
+
+echo "First Name : "
+read name
+pat="^[A-Z a-z]{2,}"
+if [[ $name == $pat ]]
+then
+  echo "$name is valid Name"
+else
+  echo "Not valid Name"
+fi
+
+echo "***********************************************"
+
 echo "Last Name : "
 read name
 pat="^[A-Z a-z]{3,}"
@@ -6,4 +21,14 @@ then
   echo "$name is valid Name"
 else
   echo "Not valid Name"
+fi
+echo "************************************************"
+echo "Enter Email ID : "
+read email
+pat2="^[a-zA-Z0-9]+[@][a-zA-Z]+[.]([a-z|A-Z]{2,}[.][a-zA-Z]{2,})|([a-zA-Z]{2,})"
+if [[ $email =~ $pat2 ]]
+then
+  echo "$email is valid Email"
+else
+  echo "Not valid Email"
 fi
